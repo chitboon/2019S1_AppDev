982b4a99b99bdff38e742a018b550874adace402
chitboon <chitboon@gmail.com>
2019-10-22 13:23:28 +0800

    Update the customer id validation

--- practical1/Customer.py
+++ practical1/Customer.py
@@ -2,7 +2,7 @@ class Customer:
 
     def __init__(self, customer_id, name):
         self.__customer_id = ''
-        self.set_customer_id(name)
+        self.set_customer_id(customer_id)
         self.__name = name
 
     def get_customer_id(self):
@@ -16,7 +16,7 @@ class Customer:
 
     def set_customer_id(self, customer_id):
         if len(customer_id) == 6:
-                if customer_id[-1].isalpha() and customer_id[0:3].isdigit():
+                if customer_id[-1].isalpha() and customer_id[0:-1].isdigit():
                     self.__customer_id = customer_id
 
     def display_details(self):
@@ -26,11 +26,11 @@ class Customer:
 
 
 
-c1 = Customer("1234A", "Andrea Tan")
+c1 = Customer("12345A", "Andrea Tan")
 c1.display_details()
 
-c2 = Customer('7895', 'Lee Chit Boon')
+c2 = Customer('78955', 'Lee Chit Boon')
 c2.display_details()
 
-c2.set_customer_id("7895B")
+c2.set_customer_id("78955B")
 c2.display_details()
